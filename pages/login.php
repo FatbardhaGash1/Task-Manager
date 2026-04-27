390c1fbca0c7b7ed55a10c0d8d2ac126f39dca78
anilamustafaa <anila.mustafa3@student.uni-pr.edu>
2026-04-27 23:04:40 +0200

    implementimi i autentikimit me session dhe cookies

--- pages/login.php
+++ pages/login.php
@@ -19,3 +19,25 @@ $valid_users = [
 ];
 
 $error = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = cleanInput($_POST['email']);
+    $password = $_POST['password'];
+
+    if(!validateEmail($email)) {
+        $error = "Email nuk është valid!";
+    } elseif(isset($valid_users[$email]) && $valid_users[$email]['password'] == $password) {
+        $_SESSION['user_id'] = $email;
+        $_SESSION['user_role'] = $valid_users[$email]['role'];
+        $_SESSION['user_name'] = $valid_users[$email]['name'];
+
+        setcookie('last_user', $email, time() + 86400 * 30, '/');
+        setcookie('theme', 'light', time() + 86400 * 30, '/');
+
+        header('Location: dashboard.php');
+        exit();
+    } else {
+        $error = "Kredenciale të pasakta!";
+    }
+}
+?>
